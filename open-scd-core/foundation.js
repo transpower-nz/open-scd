d6a4cdd31e6e0a19c0590ac73a686f2fec253993
Daniel Mulholland <dan.mulholland@gmail.com>
2024-12-06 09:20:07 +1300

    Updates

--- open-scd-core/foundation.js
+++ open-scd-core/foundation.js
@@ -1,7 +1,4 @@
 export { newOpenEvent } from './foundation/open-event.js';
-export { isNamespaced, isUpdate, newEditEventV1 } from './foundation/edit-event-v1.js';
-export { isInsert, isRemove } from './node_modules/@openenergytools/xml-lib/dist/editv2.js';
-import './node_modules/@openenergytools/xml-lib/dist/testHelpers.js';
-export { newEditEvent } from './foundation/edit-event.js';
+export { isComplex, isInsert, isNamespaced, isRemove, isUpdate, newEditEvent } from './foundation/edit-event.js';
 export { cyrb64 } from './foundation/cyrb64.js';
 //# sourceMappingURL=foundation.js.map
--- open-scd-core/locales/open-scd.spec.d.ts
+++
@@ -1,1 +0,0 @@
-import './open-scd.js';
