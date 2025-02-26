0c172ef9638f1a7e29185d3ea9e753b667d6a1f0
Daniel Mulholland <dan.mulholland@gmail.com>
2025-02-27 07:47:46 +1300

    Updates

--- plugins/oscd-open/oscd-open.d.ts
+++ plugins/oscd-open/oscd-open.d.ts
@@ -1,10 +1,7 @@
 import { LitElement } from 'lit';
-import '@material/web/progress/circular-progress.js';
 export default class OscdOpen extends LitElement {
-    showProgress: boolean;
     input: HTMLInputElement;
     run(): Promise<void>;
-    openDocs(event: Event): Promise<void>;
+    openDoc(event: Event): Promise<void>;
     render(): import("lit-html").TemplateResult<1>;
-    static styles: import("lit").CSSResult;
 }
